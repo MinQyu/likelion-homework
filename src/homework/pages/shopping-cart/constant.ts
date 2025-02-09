246bd6b6a338011cd3007a81b2aaa3b98ddf70b2
Min-gyu <jorushs@gmail.com>
2025-02-09 19:03:16 +0900

    [추가] constant 파일 추가

---
+++ src/homework/pages/shopping-cart/constant.ts
@@ -0,0 +1,39 @@
+export interface ItemType {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  img_path: string;
+}
+
+export const ITEMS: ItemType[] = [
+  {
+    id: 'item-1',
+    name: '1A 우유 900mL',
+    price: 1880,
+    stock: 2,
+    img_path: '우유.png',
+  },
+  {
+    id: 'item-2',
+    name: '맛있는 콩나물 500g',
+    price: 1280,
+    stock: 5,
+    img_path: '콩나물.png',
+  },
+  {
+    id: 'item-3',
+    name: '고소한 두부 1kg',
+    price: 2280,
+    stock: 4,
+    img_path: '두부.png',
+  },
+];
+
+export const INITAL_QUANTITIES = ITEMS.reduce(
+  (acc, item) => {
+    acc[item.id] = 1;
+    return acc;
+  },
+  {} as Record<string, number>
+);
