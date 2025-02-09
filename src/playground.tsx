5b46a109079959a0b45b6e5afa7849a5783fcd67
Min-gyu <jorushs@gmail.com>
2025-02-09 16:09:54 +0900

    [구성] 초기파일 정리

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,32 +1,12 @@
 import { useState } from 'react';
 import Nav from '@/homework/components/nav';
 import { getUIView } from '@/homework/lib/ui-view';
-import StateManagement from '@/homework/pages/state-management';
-import SignInForm from '@/homework/pages/sign-in';
-import SignUpForm from '@/homework/pages/sign-up';
-import TicTacToe from './homework/pages/tic-tac-toe';
 import ShoppingCart from './homework/pages/shopping-cart';
 
 const getViewElement = (uiView: string) => {
   let viewElement: React.ReactElement | null = null;
 
   switch (uiView) {
-    case 'signin': {
-      viewElement = <SignInForm />;
-      break;
-    }
-    case 'signup': {
-      viewElement = <SignUpForm />;
-      break;
-    }
-    case 'state-management': {
-      viewElement = <StateManagement />;
-      break;
-    }
-    case 'tic-tac-toe': {
-      viewElement = <TicTacToe />;
-      break;
-    }
     case 'shopping-cart': {
       viewElement = <ShoppingCart />;
       break;
