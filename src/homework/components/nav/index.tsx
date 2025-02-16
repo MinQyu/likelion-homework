71e3c29719d3f6984cb8746e56a1d1bed2dee796
Min-gyu <jorushs@gmail.com>
2025-02-16 17:35:42 +0900

    [구성] 더미파일 삭제 및 nav 수정

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -1,7 +1,10 @@
 import './style.css';
 import NavLink from './nav-link';
 
-const navList = [{ id: 'item-1', content: '쇼핑 카트', href: 'shopping-cart' }];
+const navList = [
+  { id: 'item-1', content: '쇼핑 카트', href: 'shopping-cart' },
+  { id: 'item-2', content: '스톱워치', href: 'stop-watch' },
+];
 
 function Nav() {
   return (
